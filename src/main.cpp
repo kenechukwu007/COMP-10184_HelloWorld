ff6968e10a20e2899e544d2dea4501235af50974
Ohia <000791775@mohawkcollege.ca>
2022-09-16 11:36:13 -0400

    Added statement of authorship

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-
+// I, William Ohia, 000791775, I acknowledge that this is my own work and 
+// no other person's work has been copied without acknowledgement. 
 unsigned long myTime;
 
 void setup() {
